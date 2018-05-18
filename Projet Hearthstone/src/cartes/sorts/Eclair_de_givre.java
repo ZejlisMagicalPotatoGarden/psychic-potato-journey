ca6696d8e1711736aeb5774a0a31714ba22ef556
louis <louis@loul>
2018-05-18 12:03:28 +0200

    j'ai oublié d'autres trucs

--- Projet Hearthstone/src/cartes/sorts/Eclair_de_givre.java
+++ Projet Hearthstone/src/cartes/sorts/Eclair_de_givre.java
@@ -7,7 +7,6 @@ public class Eclair_de_givre extends Sort {
 
 	public Eclair_de_givre() {
 		super("Eclair de givre", 2, new Degats("Inflige 3 points de dégâts au personnage ciblé", "Attaque du givre", "Immédiat", 3), "Mage");
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- Projet Hearthstone/src/cartes/sorts/Image_mirroir.java
+++ Projet Hearthstone/src/cartes/sorts/Image_mirroir.java
@@ -2,11 +2,12 @@ package cartes.sorts;
 
 import cartes.effets.Effet;
 import cartes.effets.Invocation;
+import cartes.serviteurs.Serviteur;
 
 public class Image_mirroir extends Sort {
 
 	public Image_mirroir() {
-		super("Image miroir", 1, new Invocation(), classe);
+		super("Image miroir", 1, new Invocation("Invoque deux serviteurs de Jaina 0/+2 ayant provocation", "Image mirroir", "Invocation", new Serviteur("Image miroir", 0, "?", ) , null, null), "Mage");
 	}
 
 }
